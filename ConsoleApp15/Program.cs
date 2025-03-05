1cbfcb4f0a0cf86e7e8a23b1693a63fc278f99a6
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-03-05 23:00:13 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,13 @@
+﻿double[] пиццa = new double[5]; 
+Console.WriteLine("Введите 5 чисел:");
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Элемент {i + 1}: ");
+    while (!double.TryParse(Console.ReadLine(), out пиццa[i])) ;
+}
+    Console.WriteLine("Итoг:");
+for (int i = 4; i >= 0; i--)
+{
+    Console.Write(пиццa[i] + " ");
+}
+    Console.WriteLine();
